adcd4fcf50bc35413f4869758c9495f6adbf72fa
Md Badrul Alam <developer.badrul@gmail.com>
2025-10-06 22:31:47 +0600

    add calculate Winner fn

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import Square from "./Square";
 
-const Board = () => {
+export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
+
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+        status = `Winner: ${winner}`
+    } else {
+        status = 'Next Player ' + (xIsNext ? 'O' : 'X')
+    }
 
     function handleClick(i) {
         if (squares[i]) {
@@ -13,7 +22,7 @@ const Board = () => {
         const nextSquare = squares.slice();
         if (xIsNext) {
             nextSquare[i] = 'X';
-        }else{
+        } else {
             nextSquare[i] = 'O';
         }
         setSquares(nextSquare);
@@ -22,6 +31,7 @@ const Board = () => {
 
     return (
         <div>
+            <div>{status}</div>
             <div className="flex">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -41,4 +51,23 @@ const Board = () => {
     );
 };
 
-export default Board;
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
