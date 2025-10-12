f6d0eddf8ce6367228fc205cd0d2469e53c1197c
Md Badrul Alam <developer.badrul@gmail.com>
2025-10-12 15:20:08 +0600

    add history logic

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,13 +4,39 @@ import Board from "./Board";
 const Game = () => {
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [xIsNext, setXIsNext] = useState(true);
+    const [currentMove, setCurrentMove] = useState(0);
 
-    const currentSquares = history[history.length - 1];
+    const currentSquares = history[currentMove];
 
     function handlePlay(nextSquare) {
         setXIsNext(prev => !prev);
-        setHistory([...history, nextSquare])
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquare];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1)
+        console.log('History :', nextHistory, 'CurrentMove :', currentMove);
     }
+
+    function jumpTo(move) {
+        setCurrentMove(move);
+        setXIsNext(move % 2 === 0);
+        console.log('CurrentMove :', currentMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+
+        if (move > 0) {
+            description = `Go to move # ${move}`;
+        } else {
+            description = `Go to Start the Game`
+        }
+
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+    })
 
     return (
         <div>
@@ -22,8 +48,9 @@ const Game = () => {
                 />
             </div>
             <div>
-                this will be game history
-                <ol></ol>
+                <ol>
+                    {moves}
+                </ol>
             </div>
         </div>
     );
