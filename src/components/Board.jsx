0ab116fc35ecd3e9d53d9d0753adc51586ccdb94
Md Badrul Alam <developer.badrul@gmail.com>
2025-10-13 10:49:35 +0600

    refactor Square in Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -25,24 +25,15 @@ export default function Board({ xIsNext, squares, onPlay }) {
         }
         onPlay(nextSquare)
     }
+    console.log(squares);
 
     return (
         <div>
             <div className="font-bold text-blue-500 text-2xl">{status}</div>
-            <div className="flex">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="flex">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="flex">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            <div className="grid grid-cols-3 gap-3">
+                {
+                    squares.map((square, i) => <Square key={i} value={square} onSquareClick={() => handleClick(i)} />)
+                }
             </div>
         </div>
     );
